ed915b68533822e57ce1908873a474aa24b30a83
nick <nick.jy.hsu@mail.foxconn.com>
2018-01-19 09:34:54 +0800

    Retry i2c bind command

--- node-init-sthelens/node_init_sthelens.c
+++ node-init-sthelens/node_init_sthelens.c
@@ -1,34 +1,66 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include "i2c-dev.h"
 
-/*----------------------------------------------------------------*/
-/* Main Event Loop                                                */
-#define PHYSICAL_I2C 7
-#define PSU_NUM 6
+#define MAX_I2C_DEV_LEN 32
+#define PMBUS_SLAVE_ADDR 0x58
+#define PHYSICAL_I2C 8
+#define PMBUS_NUMBER 6
 
-int
-main(int argc, char *argv[])
+void pmbus_scan()
 {
+
     char buff_path[256] = "";
-    int i = 0;
+    int i;
+    int file;
+    char filename[MAX_I2C_DEV_LEN] = {0};
+    int bus;
+    int rc = 0;
+    int res;
 
-    /* Init pmbus node */
-    for(i=1; i<=PSU_NUM; i++)
-    {
-        sprintf(buff_path, "echo pmbus 0x58 > /sys/bus/i2c/devices/i2c-%d/new_device", PHYSICAL_I2C+i);
-        printf("%s\n", buff_path);
-        system(buff_path);
+    while(1) {
+        for(i=0;i<PMBUS_NUMBER;i++) {
+            /* Init pmbus node */
+            bus = PHYSICAL_I2C + i;
+			printf ("[DEBUGMSG] bus : %d\n", bus);
+            sprintf(filename,"/dev/i2c-%d",bus);
+            file = open(filename,O_RDWR);
+            rc = ioctl(file, I2C_SLAVE, PMBUS_SLAVE_ADDR);
+			printf ("[DEBUGMSG] rc : %d\n", rc);
+            if (rc == 0) {
+                res = i2c_smbus_read_byte(file);
+				printf ("[DEBUGMSG] res : %d\n", res);
+                if (res >= 0) {
+                    sprintf(buff_path, "echo pmbus 0x58 > /sys/bus/i2c/devices/i2c-%d/new_device", bus);
+					printf ("[DEBUGMSG] buff_path : %s\n", buff_path);
+                    system(buff_path);
+					sleep(10);
+					sprintf(buff_path, "echo %d-0058 > /sys/bus/i2c/drivers/pmbus/bind", bus);
+					printf ("[DEBUGMSG] buff_path : %s\n", buff_path);
+                    system(buff_path);
+                }
+            }
+            close(file);
+        }
+        sleep(10);								// delay 10ms
     }
-
-    /* fix-mac & fix-guid */
+	printf ("[DEBUGMSG] leave while loop");
+	/* fix-mac & fix-guid */
     printf("fix-mac & fix-guid start");
     system("/usr/sbin/mac_guid.py --fix-mac");
     system("/usr/sbin/mac_guid.py --fix-guid");
+}
 
+int main(void)
+{
+    pmbus_scan();
     return 0;
 }
+
