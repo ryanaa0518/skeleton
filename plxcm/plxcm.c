76d4cbd2388318978fb5e07b088969f9aa2653a8
ryanaa0518 <ryanaa0518@gmail.com>
2018-06-11 05:38:06 -0400

    echo to /tmp

--- plxcm/plxcm.c
+++ plxcm/plxcm.c
@@ -11,7 +11,8 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sdbus_property.h>
-
+//for system call 
+#include <stdlib.h> 
 
 #define MAX_I2C_DEV_LEN 32
 #define GPU_ACCESS_SUCCESS_RETURN 0x1f
@@ -153,27 +154,28 @@ static int i2c_io(int fd, int slave_addr, int write_len, __u8 *write_data_bytes,
 	memset(&msg, 0, sizeof(msg));
 
 	if (write_len > 0) {
-                printf("\r\n Write :\r\n");
+		printf("\r\n Write :");	
 		msg[n_msg].addr = slave_addr;
-                printf("\r\n msg[n_msg].addr : 0x%x ,", msg[n_msg].addr);
+		printf("\r\n msg[n_msg].addr : 0x%x ,", msg[n_msg].addr);
 		msg[n_msg].flags = (g_use_pec) ? I2C_CLIENT_PEC : 0;
-                printf("msg[n_msg].flags :   0x%x ,",msg[n_msg].flags);
+		printf(" msg[n_msg].flags :   0x%x ,",msg[n_msg].flags);
 		msg[n_msg].len = write_len ;
-                printf("msg[n_msg].len :     0x%x ,",msg[n_msg].len);
+		printf(" msg[n_msg].len :     0x%x ,",msg[n_msg].len);
 		msg[n_msg].buf = write_data_bytes;
                 
-                printf("msg[n_msg].buf :");
-                for(i=0 ;i <msg[n_msg].len ;i ++)
-                {
-                	printf("0x%x ", msg[n_msg].buf[i]);
-                } 
+    printf(" msg[n_msg].buf :");
+    for(i=0 ;i <msg[n_msg].len ;i ++)
+    {
+    	printf("0x%x ", msg[n_msg].buf[i]);
+    } 
+    printf("\r\n");
 		n_msg++;
 	}
 
 	if (read_len>=0) {
-                printf("\r\n Read :\r\n");
+                printf("\r\n Read :");
 		msg[n_msg].addr = slave_addr;
-                printf("\r\n msg[n_msg].addr : 0x%x ,",msg[n_msg].addr);
+    printf("\r\n msg[n_msg].addr : 0x%x ,",msg[n_msg].addr);
 		msg[n_msg].flags = I2C_M_RD
 				   | ((g_use_pec) ? I2C_CLIENT_PEC : 0)
 				   | ((read_len == 0) ? I2C_M_RECV_LEN : 0);
@@ -181,12 +183,11 @@ static int i2c_io(int fd, int slave_addr, int write_len, __u8 *write_data_bytes,
 		 * In case of g_n_read is 0, block length will be added by
 		 * the underlying bus driver.
 		 */
-                printf(" msg[n_msg].flags : 0x%x ,",msg[n_msg].flags);
+    printf(" msg[n_msg].flags : 0x%x ,",msg[n_msg].flags);
 		msg[n_msg].len = (read_len) ? read_len : 256;
-                printf(" msg[n_msg].len :   0x%x ,", msg[n_msg].len);
+    printf(" msg[n_msg].len :   0x%x ,", msg[n_msg].len);
 		msg[n_msg].buf = read_data_bytes ;
  
-
 		n_msg++;
 	}
         
@@ -199,15 +200,18 @@ static int i2c_io(int fd, int slave_addr, int write_len, __u8 *write_data_bytes,
 		return -1;
 	}
 	
-		printf(" msg[n_msg-1].buf :  ");
+	printf(" msg[n_msg-1].buf :");
 		
-		//ryan: after the read , print the read message
-		for(i=0 ;i <msg[n_msg-1].len ;i ++)
-		{
-		        printf("0x%x ", msg[n_msg-1].buf[i]);
-		}
-                	
+	//ryan: after the read , print the read message
+	for(i=0 ;i <msg[n_msg-1].len ;i ++)
+	{
+	        printf("0x%02x ", msg[n_msg-1].buf[i]);
+	}
+  
+  printf("\r\n\r\n");            	
 	return 0;
+	
+	
 }
 
 static i2c_raw_access(int i2c_bus, int i2c_addr,int write_len, __u8 *write_data_bytes, int read_len, __u8 *read_data_bytes)
@@ -256,11 +260,11 @@ static int smbus_commmand_write(int i2c_bus, int i2c_addr, __u8 *write_data, int
 	int w_count = 0;
 	int i;
 
-	sprintf(filename,"/dev/i2c-%d",i2c_bus);
+	//sprintffilename,"/dev/i2c-%d",i2c_bus);
 	fd = open(filename,O_RDWR);
 
 	if (fd == -1) {
-		fprintf(stderr, "Failed to open i2c device %s", filename);
+		//fprintf(stderr, "Failed to open i2c device %s", filename);
 		return rc;
 	}
 
@@ -340,7 +344,7 @@ void write_file_pex(int pex_idx, double data, char *sub_name)
 	}
 	
 
-	sprintf(f_path, "%s/pex%d_%s", PEX_TEMP_PATH, pex_idx, sub_name);
+	sprintf(f_path, "%s/pex%d_%s", PEX_TEMP_PATH, pex_idx+1, sub_name);
 	sprintf(sys_cmd, "echo %d > %s", (int)data, f_path);
 	system(sys_cmd);
 }
@@ -367,7 +371,7 @@ int function_get_pex_temp_data(int pex_idx)
 			     i2c_cmd->write_cmd.data, i2c_cmd->read_cmd.len, i2c_cmd->read_cmd.data);
 	if (ret < 0) {
 		real_temp_data = -1;
-		fprintf(stderr, "Failed to do iotcl cmd:%d, ret:%d\n", i2c_cmd->cmd, ret);
+		//fprintf(stderr, "Failed to do iotcl cmd:%d, ret:%d\n", i2c_cmd->cmd, ret);
 		goto error_i2c_access;
 	}
   
@@ -377,7 +381,7 @@ int function_get_pex_temp_data(int pex_idx)
 			     i2c_cmd->write_cmd.data, i2c_cmd->read_cmd.len, i2c_cmd->read_cmd.data);
 	if (ret < 0) {
 		real_temp_data = -1;
-		fprintf(stderr, "Failed to do iotcl cmd:%d, ret=%d\n", i2c_cmd->cmd, ret);
+		//fprintf(stderr, "Failed to do iotcl cmd:%d, ret=%d\n", i2c_cmd->cmd, ret);
 		goto error_i2c_access;
 	}
 
@@ -387,7 +391,7 @@ int function_get_pex_temp_data(int pex_idx)
 			     i2c_cmd->write_cmd.data, i2c_cmd->read_cmd.len, i2c_cmd->read_cmd.data);
 	if (ret < 0) {
 		real_temp_data = -1;
-		fprintf(stderr, "Failed to do iotcl cmd:%d, ret:%d\n", i2c_cmd->cmd, ret);
+		//fprintf(stderr, "Failed to do iotcl cmd:%d, ret:%d\n", i2c_cmd->cmd, ret);
 		goto error_i2c_access;
 	}
 
@@ -395,7 +399,7 @@ int function_get_pex_temp_data(int pex_idx)
 	rx_data = i2c_cmd->read_cmd.data;
 
 	if ((rx_data[0] & 0x80) != 0x80) {
-		//printf("[DEBUGMSG][pex9797] rx_data[0]:%s\n", rx_data[0]);
+		////printf("[DEBUGMSG][pex9797] rx_data[0]:%s\n", rx_data[0]);
 		real_temp_data = -1;
 		goto error_i2c_access;
 	}
@@ -414,13 +418,13 @@ error_i2c_access:
 
 //add by ryan
 
-int function_write_reg_data(int pex_idx,__u16 reg,__u32 data)
+int function_write_reg_data(int pex_idx,int port_id ,__u16 reg,__u32 data)
 {
 	pex_device_i2c_cmd *i2c_cmd;
 	int i2c_bus;
 	int i2c_addr;
 	int ret;
-	int i;
+	int i,station,port;
 	char *rx_data;
 	int rx_len = 0;
 	unsigned int temp_sensor_data = 0;
@@ -430,108 +434,115 @@ int function_write_reg_data(int pex_idx,__u16 reg,__u32 data)
 	i2c_bus = pex_device_bus[pex_idx].bus_no;
 	i2c_addr = pex_device_bus[pex_idx].slave;
 
-	printf("\r\n reg : 0x%x \r\n",reg);
-	printf("\r\n reg : 0x%x \r\n",data);
-	
+	//printf("\r\n reg : 0x%x \r\n",reg);
+	//printf("\r\n port_id : 0x%x \r\n",port_id);
+	//printf("\r\n data : 0x%x \r\n",data);
+	station = (port_id>>2)  ;
+	port = port_id & 3 ;
 	
 		
 	//EM_PEX_CMD_TEMP_READ : 2
 	i2c_cmd = &pex_device_cmd_tab[EM_PEX_CMD_TEMP_DISABLE_CTL];	
 	
 	i2c_cmd->write_cmd.data[0]= 0x03; //write command
-	i2c_cmd->write_cmd.data[1]= 0x00; //Non-Fabric Mode
+	i2c_cmd->write_cmd.data[1]= ((station&7)<<2) | ((port>>1)&3); //Non-Fabric Mode
 	
-	i2c_cmd->write_cmd.data[2]= ((reg>>10)&0xff) | 0x3c;
-	printf("\r\n i2c_cmd->write_cmd.data[2] : %x \r\n",i2c_cmd->write_cmd.data[2]);
+	i2c_cmd->write_cmd.data[2]= (port&1)<<7 | 0x3c | ((reg>>10) & 3);
+	//printf("\r\n i2c_cmd->write_cmd.data[2] : %x \r\n",i2c_cmd->write_cmd.data[2]);
 	i2c_cmd->write_cmd.data[3]= (reg>>2)&0xff;
-	printf("\r\n i2c_cmd->write_cmd.data[3] : %x \r\n",i2c_cmd->write_cmd.data[3]);
+	//printf("\r\n i2c_cmd->write_cmd.data[3] : %x \r\n",i2c_cmd->write_cmd.data[3]);
 	
 	//data byte
 	i2c_cmd->write_cmd.data[4]= (data & 0xff);
-	printf("\r\n i2c_cmd->write_cmd.data[4] : %x \r\n",i2c_cmd->write_cmd.data[4]);
+	//printf("\r\n i2c_cmd->write_cmd.data[4] : %x \r\n",i2c_cmd->write_cmd.data[4]);
 	
 	i2c_cmd->write_cmd.data[5]= ((data >>8)& 0xff);
-	printf("\r\n i2c_cmd->write_cmd.data[5] : %x \r\n",i2c_cmd->write_cmd.data[5]);
+	//printf("\r\n i2c_cmd->write_cmd.data[5] : %x \r\n",i2c_cmd->write_cmd.data[5]);
 
 	i2c_cmd->write_cmd.data[6]= ((data >>16)& 0xff);
-	printf("\r\n i2c_cmd->write_cmd.data[6] : %x \r\n",i2c_cmd->write_cmd.data[6]);
+	//printf("\r\n i2c_cmd->write_cmd.data[6] : %x \r\n",i2c_cmd->write_cmd.data[6]);
 	
 	i2c_cmd->write_cmd.data[7]= ((data >>24)& 0xff);
-	printf("\r\n i2c_cmd->write_cmd.data[7] : %x \r\n",i2c_cmd->write_cmd.data[7]);
+	//printf("\r\n i2c_cmd->write_cmd.data[7] : %x \r\n",i2c_cmd->write_cmd.data[7]);
 	
 				
 	ret = i2c_raw_access(i2c_bus, i2c_addr, i2c_cmd->write_cmd.len,
 			     i2c_cmd->write_cmd.data, i2c_cmd->read_cmd.len, i2c_cmd->read_cmd.data);
 	if (ret < 0) {
-		real_temp_data = -1;
-		fprintf(stderr, "Failed to do iotcl cmd:%d, ret:%d\n", i2c_cmd->cmd, ret);
-		goto error_i2c_access;
+		printf("\r\n-----Fail to get thd data-----\r\n");
 	}
 
-#if 0
-	rx_len = i2c_cmd->read_cmd.len;
-	rx_data = i2c_cmd->read_cmd.data;
-	
-	for(i=0;i<rx_len;i++)
-	{
-		printf("\r\n i2c_cmd->read_cmd.data[%d] : %x \r\n",i,i2c_cmd->read_cmd.data[i]);
-	}
-#endif	
 
-error_i2c_access:
-	write_file_pex(pex_idx, real_temp_data, "temp");
-	return rc ;
 }
 
 
-int function_read_reg_data(int pex_idx,__u16 reg)
+int function_read_reg_data(int pex_idx ,int port_id ,__u16 reg)
 {
 	pex_device_i2c_cmd *i2c_cmd;
 	int i2c_bus;
 	int i2c_addr;
 	int ret;
-	int i;
+	int i,station,port;
 	char *rx_data;
 	int rx_len = 0;
 	unsigned int temp_sensor_data = 0;
 	double real_temp_data = 0.0;
 	int rc=-1;
+	char shell_cmd[128]="";
+	__u32 regData= 0x00000000;
 
 	i2c_bus = pex_device_bus[pex_idx].bus_no;
 	i2c_addr = pex_device_bus[pex_idx].slave;
 
-	printf("\r\n reg : 0x%x \r\n",reg);
+	//printf("\r\n reg : 0x%x \r\n",reg);
+	//printf("\r\n port_id : 0x%x \r\n",port_id);
+	station = (port_id>>2)  ;
+	port = port_id & 3 ;
 	
 		
 	//EM_PEX_CMD_TEMP_READ : 2
 	i2c_cmd = &pex_device_cmd_tab[EM_PEX_CMD_TEMP_READ];	
 	
-	i2c_cmd->write_cmd.data[2]= ((reg>>10)&0xff) | 0x3c;
-	printf("\r\n i2c_cmd->write_cmd.data[2] : %x \r\n",i2c_cmd->write_cmd.data[2]);
+	i2c_cmd->write_cmd.data[1]= ((station&7)<<2) | ((port>>1)&3); //Non-Fabric Mode	
+	i2c_cmd->write_cmd.data[2]= (port&1)<<7 | 0x3c | ((reg>>10) & 3);
+	//printf("\r\n i2c_cmd->write_cmd.data[2] : %x \r\n",i2c_cmd->write_cmd.data[2]);
 	i2c_cmd->write_cmd.data[3]= (reg>>2)&0xff;
-	printf("\r\n i2c_cmd->write_cmd.data[3] : %x \r\n",i2c_cmd->write_cmd.data[3]);
+	//printf("\r\n i2c_cmd->write_cmd.data[3] : %x \r\n",i2c_cmd->write_cmd.data[3]);
 			
 	ret = i2c_raw_access(i2c_bus, i2c_addr, i2c_cmd->write_cmd.len,
 			     i2c_cmd->write_cmd.data, i2c_cmd->read_cmd.len, i2c_cmd->read_cmd.data);
 	if (ret < 0) {
-		real_temp_data = -1;
-		fprintf(stderr, "Failed to do iotcl cmd:%d, ret:%d\n", i2c_cmd->cmd, ret);
-		goto error_i2c_access;
+		printf("\r\n-----Fail to get thd data-----\r\n");
 	}
-
-#if 0
-	rx_len = i2c_cmd->read_cmd.len;
-	rx_data = i2c_cmd->read_cmd.data;
 	
-	for(i=0;i<rx_len;i++)
-	{
-		printf("\r\n i2c_cmd->read_cmd.data[%d] : %x \r\n",i,i2c_cmd->read_cmd.data[i]);
-	}
+	//touch /tmp/pex/0x80
+	//echo 0xff > /tmp/pex/0x80
+	
+
+
+#if 1
+//save the reg data in file system
+
+	sprintf (shell_cmd, "mkdir /tmp/pex/%d/",pex_idx);
+	system(shell_cmd);
+	
+	sprintf (shell_cmd, "mkdir /tmp/pex/%d/%d",pex_idx,port_id);
+	system(shell_cmd);
+		
+	sprintf (shell_cmd, "touch /tmp/pex/%d/%d/0x%08x",pex_idx,port_id,reg);
+	system(shell_cmd);
+		
+	regData = ((i2c_cmd->read_cmd.data[0]) << 24) |
+						((i2c_cmd->read_cmd.data[1]) << 16) |
+						((i2c_cmd->read_cmd.data[2]) << 8) |
+						(i2c_cmd->read_cmd.data[3]);
+
+	
+	sprintf (shell_cmd, "echo 0x%08x > /tmp/pex/%d/%d/0x%08x",regData,pex_idx,port_id,reg);
+	system(shell_cmd);
+		
 #endif
 
-error_i2c_access:
-	write_file_pex(pex_idx, real_temp_data, "temp");
-	return rc ;
 }	
 
 
@@ -542,7 +553,7 @@ int pex_set_dbus_property(int pex_idx, char *property_name, char *property_value
 	int rc;
 	rc = sd_bus_open_system(&bus);
 	if(rc < 0) {
-		fprintf(stderr,"Error opening system bus.\n");
+		//fprintf(stderr,"Error opening system bus.\n");
 		return rc;
 	}
 
@@ -577,7 +588,7 @@ void function_get_pex_serial_data(int pex_idx)
 	ret = i2c_raw_access(i2c_bus, i2c_addr, i2c_cmd->write_cmd.len,
 			     i2c_cmd->write_cmd.data, i2c_cmd->read_cmd.len, i2c_cmd->read_cmd.data);
 	if (ret < 0) {
-		fprintf(stderr, "Failed to do iotcl I2C_SLAVE, cmd:%d, ret:%d\n", i2c_cmd->cmd, ret);
+		//fprintf(stderr, "Failed to do iotcl I2C_SLAVE, cmd:%d, ret:%d\n", i2c_cmd->cmd, ret);
 		return ;
 	}
 
@@ -590,7 +601,7 @@ void function_get_pex_serial_data(int pex_idx)
 	ret = i2c_raw_access(i2c_bus, i2c_addr, i2c_cmd->write_cmd.len,
 			     i2c_cmd->write_cmd.data, i2c_cmd->read_cmd.len, i2c_cmd->read_cmd.data);
 	if (ret < 0) {
-		fprintf(stderr, "Failed to do iotcl I2C_SLAVE, cmd:%d, ret:%d\n", i2c_cmd->cmd, ret);
+		//fprintf(stderr, "Failed to do iotcl I2C_SLAVE, cmd:%d, ret:%d\n", i2c_cmd->cmd, ret);
 		return ;
 	}
 
@@ -599,11 +610,11 @@ void function_get_pex_serial_data(int pex_idx)
 
 	st = property_value;
 	for (i = 0; i<p_serial->serial_count; i++, st+=2) {
-		snprintf(st, 3, "%02x", p_serial->serial_data[i]);
+		//snprintf(st, 3, "%02x", p_serial->serial_data[i]);
 	}
 	
 //add by ryan
-  printf("\r\n--function_get_pex_serial_data , st : %02x ---  \r\n",st);
+  //printf("\r\n--function_get_pex_serial_data , st : %02x ---  \r\n",st);
 
 	if (pex_set_dbus_property(pex_idx, "Serial Number", property_value) < 0)
 		p_serial->serial_count = 0;
@@ -629,10 +640,10 @@ void function_get_pex_udid_data(int pex_idx)
 			//set pex9797 dbus property
 			st = property_value;
 			for (i = 0; i<p_udid->udid_count; i++, st+=2) {
-				snprintf(st, 3, "%02x", p_udid->udid_data[i]);
+				//snprintf(st, 3, "%02x", p_udid->udid_data[i]);
 			}
 			//add by ryan
-  		printf("\r\n--function_get_pex_udid_data, st : %02x ---  \r\n",st);
+  		//printf("\r\n--function_get_pex_udid_data, st : %02x ---  \r\n",st);
 			if (pex_set_dbus_property(pex_idx, "UDID", property_value) >= 0)
 					p_udid->udid_sdbus_flag = 1;
 		}
@@ -644,7 +655,7 @@ void function_get_pex_udid_data(int pex_idx)
 	ret = i2c_raw_access(i2c_bus, i2c_addr, i2c_cmd->write_cmd.len,
 			     i2c_cmd->write_cmd.data, i2c_cmd->read_cmd.len, i2c_cmd->read_cmd.data);
 	if (ret < 0) {
-		fprintf(stderr, "Failed to do iotcl I2C_SLAVE, cmd:%d, ret:%d\n", i2c_cmd->cmd, ret);
+		//fprintf(stderr, "Failed to do iotcl I2C_SLAVE, cmd:%d, ret:%d\n", i2c_cmd->cmd, ret);
 		return ;
 	}
 
@@ -656,7 +667,7 @@ void function_get_pex_udid_data(int pex_idx)
 	ret = i2c_raw_access(i2c_bus, i2c_addr, i2c_cmd->write_cmd.len,
 			     i2c_cmd->write_cmd.data, i2c_cmd->read_cmd.len, i2c_cmd->read_cmd.data);
 	if (ret < 0) {
-		fprintf(stderr, "Failed to do iotcl I2C, cmd:%d, ret:%d\n", i2c_cmd->cmd, ret);
+		//fprintf(stderr, "Failed to do iotcl I2C, cmd:%d, ret:%d\n", i2c_cmd->cmd, ret);
 		goto out_pex_udid_data;
 	}
 
@@ -666,12 +677,12 @@ void function_get_pex_udid_data(int pex_idx)
 	//set pex9797 dbus property
 	st = property_value;
 	for (i = 0; i<p_udid->udid_count; i++, st+=2) {
-		printf("\r\n p_udid->udid_data[i] : %x \r\n",p_udid->udid_data[i]);
-		snprintf(st, 3, "%02x", p_udid->udid_data[i]);
+		//printf("\r\n p_udid->udid_data[i] : %x \r\n",p_udid->udid_data[i]);
+		//snprintf(st, 3, "%02x", p_udid->udid_data[i]);
 	}
 
 	//add by ryan
-  printf("\r\n--function_get_pex_udid_data 2, st : %02x ---  \r\n",st);
+  //printf("\r\n--function_get_pex_udid_data 2, st : %02x ---  \r\n",st);
   
 	p_udid->udid_sdbus_flag = 1;
 	if (pex_set_dbus_property(pex_idx, "UDID", property_value) < 0)
@@ -688,16 +699,16 @@ int  init_data_folder(int index)
 {
 	char f_path[128];
 	FILE *fp;
-	sprintf(f_path, "%s/pex%d_temp", PEX_TEMP_PATH, index);
+	//sprintff_path, "%s/pex%d_temp", PEX_TEMP_PATH, index);
 	if( access( f_path, F_OK ) != -1 )
 		return 1;
 	else {
 		fp = fopen(f_path,"w");
 		if(fp == NULL) {
-			fprintf(stderr,"Error:[%s] opening:[%s]\n",strerror(errno),f_path);
+			//fprintf(stderr,"Error:[%s] opening:[%s]\n",strerror(errno),f_path);
 			return -1;
 		}
-		fprintf(fp, "%d",-1);
+		//fprintf(fp, "%d",-1);
 		fclose(fp);
 	}
 	return 1;
@@ -717,7 +728,7 @@ static void notify_device_ready(char *obj_path)
     sd_bus *bus = NULL;
     rc = sd_bus_open_system(&bus);
     if(rc < 0) {
-        fprintf(stderr,"Error opening system bus.\n");
+        //fprintf(stderr,"Error opening system bus.\n");
         return ;
     }
     rc = set_dbus_property(bus, obj_path, "ready", "i", (void *) &val, -1);
@@ -744,7 +755,7 @@ void pex_data_scan()
 			return ;
 	}
 
-	printf("pex9797 control starting!!!\n");
+	//printf("pex9797 control starting!!!\n");
 	while(1) {
 		for(i=1; i<=MAX_PEX_NUM; i++) { //[DEBUGMSG]
 			rc =  function_get_pex_temp_data(i);
@@ -763,10 +774,10 @@ static void save_pid (void) {
     FILE *pidfile = NULL;
     pid = getpid();
     if (!(pidfile = fopen("/run/pex_core.pid", "w"))) {
-        fprintf(stderr, "failed to open pidfile\n");
+        //fprintf(stderr, "failed to open pidfile\n");
         return;
     }
-    fprintf(pidfile, "%d\n", pid);
+    //fprintf(pidfile, "%d\n", pid);
     fclose(pidfile);
 }
 
@@ -775,13 +786,14 @@ void usage() {
 	printf("The plxcm usage:\r\n");
 	printf("=================================================\r\n");
   printf("\n version : %s \n", VERSION);
-  printf("Usage: plxcm [-d device_id] [-a address]  [-r|-w value] \n");
+  printf("Usage: plxcm [-d device_id] [-p port_id] [-a address]  [-r|-w value] \n");
   printf("       -d : device id \n");
+  printf("       -p : assign port \n");
   printf("       -a : assign address \n");
   printf("       -r : read  \n");
   printf("       -w  [value]:  write value   \n");  	
-	printf("   such as  plxcm.exe -d 1 -a 0xbac -r 1 \r\n");
-	printf("   such as  plxcm.exe -d 1 -a 0x80 -w 0xff  \r\n");
+	printf("   such as  plxcm.exe -d 1 -p 0 -a 0xbac -r 1 \r\n");
+	printf("   such as  plxcm.exe -d 1 -p 0 -a 0x80  -w 0xff  \r\n");
 	printf("=================================================\r\n");
 	printf("Device Id List\r\n");
 	printf("-d = 0 :  16, 0x5d, EM_PEX_DEVICE_1 \r\n");
@@ -796,7 +808,7 @@ int
 main(int argc, char** argv)
 {
 	int i;
-	int portArg,regArg;
+	int devArg,portArg,regArg;
 	__u32 regData;
 					
 	//if the input parameter is fewer , show warning message
@@ -808,53 +820,63 @@ main(int argc, char** argv)
 	
 	for(i=0;i<argc;i++)
 	{
-		printf("\r\nargv[%d]: %s\r\n",i,argv[i]);	
+		//printf("\r\nargv[%d]: %s\r\n",i,argv[i]);	
 	}
 	  
 	while (argc > 2) {
     char* const opt = argv[1];
-    printf("\r\nargv[1] : %s \r\n" ,argv[1]);
+    //printf("\r\nargv[1] : %s \r\n" ,argv[1]);
     
     
     if (opt[0] != '-') usage();
-    //printf("\r\n opt[0] :%s  , opt[1] :%s\r\n",&opt[0],&opt[1]);
+    ////printf("\r\n opt[0] :%s  , opt[1] :%s\r\n",&opt[0],&opt[1]);
     
     switch (opt[1]) {
     	
     	  
 				case 'd': { // specify device id
-					printf("\r\nSetting the device id:");
+					//printf("\r\nSetting the device id:");
+			
+					if (sscanf(argv[2], "%d", &devArg) != 1) {
+						usage();
+					}
+				
+				//printf("%d \r\n",devArg);
+				argv=argv+2; 
+				argc=argc-2;
+				break;
+				}
+				case 'p': { // specify port number
+					//printf("\r\nSetting the port:");
 			
 					if (sscanf(argv[2], "%d", &portArg) != 1) {
 						usage();
 					}
 				
-				printf("%d \r\n",portArg);
-				//++argv; --argc;
+				//printf("%d \r\n",portArg);
 				argv=argv+2; 
 				argc=argc-2;
 				break;
 				}
+				
 
 				case 'a': { // specify the register
-					printf("\r\nSetting the register:\r\n");
+					//printf("\r\nSetting the register:");
 			
 					if (sscanf(argv[2], "%x", &regArg) != 1) {
 						usage();
 					}
 				
-				printf("%x \r\n",regArg);
-				//++argv; --argc;
+				//printf("0x%x \r\n",regArg);
 				argv=argv+2; 
 				argc=argc-2;
 				break;
 				}
 
 				case 'r': { // read the register
-					printf("\r\nReading the register\r\n");				
+					//printf("\r\nReading the register\r\n");				
 	
-				function_read_reg_data(portArg,regArg);
-				//++argv; --argc;
+				function_read_reg_data(devArg,portArg,regArg);
 				argv=argv+2; 
 				argc=argc-2;
 				break;
@@ -862,16 +884,15 @@ main(int argc, char** argv)
 
 
 				case 'w': { // write the register
-					printf("\r\nWriteing the register:\r\n");				
+					//printf("\r\nWriteing the register:\r\n");				
 	
 					if (sscanf(argv[2], "%x", &regData) != 1) {
 						usage();
 					}
 				
-					printf("%x \r\n",regData);
+					//printf("%x \r\n",regData);
 				
-				function_write_reg_data(portArg,regArg,regData);
-				//++argv; --argc;
+				function_write_reg_data(devArg,portArg,regArg,regData);
 				argv=argv+2; 
 				argc=argc-2;
 				break;
